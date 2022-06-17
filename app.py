ebc5012abd4a0452d9bd41f5d3211104847599c6
mattmuroya <matt.muroya@gmail.com>
2022-06-16 22:01:38 -1000

    initialize data and endpoints

--- app.py
+++ app.py
@@ -1,10 +1,44 @@
-from flask import Flask
+from flask import Flask, jsonify, request
+import data
+
 app = Flask(__name__)
+app.url_map.strict_slashes = False
 
 
 @app.route("/")
 def aloha_world():
     return "aloha, world!"
+
+
+# /api/islands
+
+
+@app.route("/api/islands")
+def get_all_islands():
+    return jsonify(data.islands)
+
+
+@app.route("/api/islands/<island_id>")
+def get_island_by_id(island_id):
+    for island in data.islands:
+        if island["id"] == int(island_id):
+            return island
+
+
+# /api/parks
+
+
+@app.route("/api/parks")
+def get_all_parks():
+    island = request.args.get('island')
+    return jsonify(data.parks)
+
+
+@app.route("/api/parks/<park_id>")
+def get_park_by_id(park_id):
+    for park in data.parks:
+        if park["id"] == int(park_id):
+            return park
 
 
 app.run(use_reloader=True)
---
+++ data.py
@@ -0,0 +1,73 @@
+islands = [
+    {
+        "id": 1,
+        "name": "Kauaʻi",
+        "nameCanonical": "kauai",
+        "numberOfParks": 9
+    },
+    {
+        "id": 2,
+        "name": "Oʻahu",
+        "nameCanonical": "oahu",
+        "numberOfParks": 20
+    }
+]
+
+parks = [
+
+    # KAUAʻI
+
+    {
+        "id": 1,
+        "name": "Ahukini State Recreational Pier",
+        "islandId": 1
+    },
+    {
+        "id": 2,
+        "name": "Hāʻena State Park",
+        "islandId": 1
+    },
+    {
+        "id": 3,
+        "name": "Kōkeʻe State Park",
+        "islandId": 1
+    },
+    {
+        "id": 4,
+        "name": "Nāpali Coast State Wilderness Park",
+        "islandId": 1
+    },
+    {
+        "id": 5,
+        "name": "Polihale State Park",
+        "islandId": 1
+    },
+    {
+        "id": 6,
+        "name": "Russian Fort Elizabeth State Historical Park",
+        "islandId": 1
+    },
+    {
+        "id": 7,
+        "name": "Wailua River State Park",
+        "islandId": 1
+    },
+    {
+        "id": 8,
+        "name": "Waimea Canyon State Park",
+        "islandId": 1
+    },
+    {
+        "id": 9,
+        "name": "Waimea State Recreational Pier",
+        "islandId": 1
+    },
+
+    # OʻAHU
+
+    {
+        "id": 10,
+        "name": "Ahupuaʻa ʻO Kahana State Park",
+        "islandId": 2
+    }
+]
